7f836d329ca9c697a7c36f78f0db79739ce88c92
Suriya KS <suriya2002.cse@gmail.com>
2021-05-30 12:59:20 +0530

    Implemented basic dropdown functionality

--- js/script.js
+++ js/script.js
@@ -14,3 +14,11 @@
         proximity: 110,
     });
 })(jQuery);
+
+const dropdownOption = document.querySelector('.dropdown__selected-option');
+
+document.querySelector('.dropdown__list').addEventListener('click', event => {
+    if (event.target.classList.contains('dropdown__option')) {
+        dropdownOption.innerHTML = event.target.innerHTML;
+    }
+});
