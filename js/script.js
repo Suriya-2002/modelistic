d4dd2c8a86294cc0fac85901a2afc02d15e6c7df
Suriya KS <suriya2002.cse@gmail.com>
2021-05-31 08:39:29 +0530

    Shifted styleshout js code to the relevant file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-(function ($) {
-    $(window).load(function () {
-        $('#loader').fadeOut('slow', function () {
-            $('#preloader').delay(300).fadeOut('slow');
-        });
-    });
-
-    $('.home-particles').particleground({
-        dotColor: '#fff',
-        lineColor: '#777',
-        particleRadius: 6,
-        curveLines: true,
-        density: 10000,
-        proximity: 110,
-    });
-})(jQuery);
-
 const dropdownOption = document.querySelector('.dropdown__selected-option');
 
 document.querySelector('.dropdown__list').addEventListener('click', event => {
