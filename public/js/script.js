752796865d1b346e836d9667aca6d682b2b50869
Suriya KS <suriya2002.cse@gmail.com>
2021-06-01 18:51:43 +0530

    Implemented pop-up functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,21 @@
-const dropdownOption = document.querySelector('.dropdown__selected-option');
+const popUp = document.querySelector('.pop-up');
 
 document.querySelector('.dropdown__list').addEventListener('click', event => {
     if (event.target.classList.contains('dropdown__option')) {
-        dropdownOption.innerHTML = event.target.innerHTML;
+        document.querySelector('.dropdown__selected-option').innerHTML = event.target.innerHTML;
     }
+});
+
+document.querySelector('.model-cards').addEventListener('click', event => {
+    if (event.target.classList.contains('model-card__button')) {
+        popUp.classList.add('pop-up--active');
+    }
+});
+
+document.querySelector('.pop-up__overlay').addEventListener('click', () => {
+    popUp.classList.remove('pop-up--active');
+});
+
+document.querySelector('.pop-up__button').addEventListener('click', () => {
+    popUp.classList.remove('pop-up--active');
 });
